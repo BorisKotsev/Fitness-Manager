59c45edfddced81fd7627d393203caf062fb1b32
Kiril Mitzev <kiril.mitzev@gmail.com>
2022-03-19 00:08:55 +0200

    Add project files.

---
+++ FitManager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FitManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ FitManager/Models/ManagerDbContext.cs
@@ -0,0 +1,20 @@
+﻿using FitManager.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitManager.Models
+{
+    public class ManagerDbContext : IdentityDbContext<User, IdentityRole<int>, int>
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public ManagerDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
